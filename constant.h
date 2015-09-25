386424adb574f290e17c4cfd4948a3024468b4a4
remis2 <remis2@illinois.edu>
2015-09-25 10:35:29 -0500

    spread and detect files added

--- constant.h
+++ constant.h
@@ -1,6 +1,26 @@
 #ifndef CONSTANT_H
 #define CONSTANT_H
 
+typedef enum MessageType
+{
+    MSG_ACK,
+    MSG_PING,
+    MSG_PIGGY,
+    MSG_FAIL,
+    MSG_JOIN,
+    MSG_LEAVE
+} messageType;
+
+struct Message {
+
+	messageType type;
+	uint8_t roundId;
+	int senderAdd[4];
+	int carrierAdd[4];
+	int timeStamp;
+	char TTL;
+};
+
 #define BUFFER_MAX (1024*1024*8)
 #define NODES_NUMBER (7)
 
